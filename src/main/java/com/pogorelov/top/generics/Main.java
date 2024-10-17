af0ead8accf267ad049677c6d636b996dce7aed9
Oleg Pogorelov <89607888555@mail.ru>
2024-10-18 01:45:44 +0700

    fix

--- src/main/java/com/pogorelov/top/generics/Main.java
+++ src/main/java/com/pogorelov/top/generics/Main.java
@@ -2,46 +2,45 @@ package com.pogorelov.top.generics;
 
 public class Main {
     public static void main(String[] args) {
-        MyArrayList<Integer> ma = new MyArrayList<>();
+        MyArrayList<Integer> myArrayList = new MyArrayList<>();
 
-        ma.pushBack(1);
-        ma.pushBack(2);
-        ma.pushBack(3);
-        ma.pushBack(4);
-        ma.pushBack(5);
-        ma.pushBack(6);
-        ma.pushBack(7);
-        ma.pushBack(8);
-        ma.pushBack(9);
-        ma.pushBack(10);
-        System.out.println(ma + " pushBack 1-10");
+        myArrayList.pushBack(1);
+        myArrayList.pushBack(2);
+        myArrayList.pushBack(3);
+        myArrayList.pushBack(4);
+        myArrayList.pushBack(5);
+        myArrayList.pushBack(6);
+        myArrayList.pushBack(7);
+        myArrayList.pushBack(8);
+        myArrayList.pushBack(9);
+        myArrayList.pushBack(10);
+        System.out.println(myArrayList + " pushBack 1-10");
 
-        ma.pushFront(0);
-        System.out.println(ma+ " pushFront 0");
+        myArrayList.pushFront(0);
+        System.out.println(myArrayList + " pushFront 0");
 
+        myArrayList.popFront();
+        System.out.println(myArrayList + " popFront");
 
-        ma.popFront();
-        System.out.println(ma + " popFront");
+        myArrayList.insert(2, -1);
+        myArrayList.insert(4, -1);
+        myArrayList.insert(6, -1);
+        System.out.println(myArrayList + " insert index 2,4,6 el -1");
 
-        ma.insert(2, -1);
-        ma.insert(4, -1);
-        ma.insert(6, -1);
-        System.out.println(ma + " insert index 2,4,6 el -1");
+        myArrayList.removeAt(0);
+        System.out.println(myArrayList + " removeAt index 0");
 
-        ma.removeAt(0);
-        System.out.println(ma + " removeAt index 0");
+        myArrayList.remove(-1);
+        System.out.println(myArrayList + " remove -1");
 
-        ma.remove(-1);
-        System.out.println(ma + " remove -1");
+        myArrayList.removeAll(-1);
+        System.out.println(myArrayList + " removeAll -1");
 
-        ma.removeAll(-1);
-        System.out.println(ma + " removeAll -1");
+        myArrayList.popBack();
+        System.out.println(myArrayList + " popBack");
 
-        ma.popBack();
-        System.out.println(ma + " popBack");
-
-        ma.clear();
-        System.out.println(ma + " clear");
-        System.out.println(ma.getSize()+ " getSize");
+        myArrayList.clear();
+        System.out.println(myArrayList + " clear");
+        System.out.println(myArrayList.getSize() + " getSize");
     }
 }
